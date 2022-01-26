6c2c9d5f3b7aebbfb64da0ec9a547abc6592dfa6
Kunal Sirpor <kunalsirpor2001@gmail.com>
2022-01-26 23:51:20 +0530

    started with App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-// work in progress
+import React from "react";
+
+import { CssBaseline, Grid } from "@material-ui/core";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+//import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
