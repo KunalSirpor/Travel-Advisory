103e385f87a2bc102bf40fe0bf7109765af1c68c
Kunal Sirpor <kunalsirpor2001@gmail.com>
2022-01-27 15:46:15 +0530

    added header / map comp.

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography } from "@material-ui/core"; //useMediaQuery;
+import { Rating } from "@material-ui/lab";
+import { LocationOnOutlined } from "@material-ui/icons/LocationOnOutlined";
+
+import useStyles from "./styles";
 
 const Map = () => {
-  return <div>Map</div>;
+  const classes = useStyles();
+
+  const coordinates = { lat: -23.5489, lng: -46.6388 };
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={15}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChnage={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
 };
 
 export default Map;
