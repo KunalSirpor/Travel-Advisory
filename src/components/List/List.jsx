dd4c92e8b35b3878159870038d87d330228ef5af
Kunal Sirpor <kunalsirpor2001@gmail.com>
2022-01-28 23:05:06 +0530

    Add List Component

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { useState } from "react";
+import {
+  Typography,
+  InputLabel,
+  Select,
+  FormControl,
+  MenuItem,
+} from "@material-ui/core";
+
+import useStyles from "./styles";
 
 const List = () => {
-  return <div>List</div>;
+  const classes = useStyles();
+  const [type, setType] = useState("restaurants");
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4">Restaurants</Typography>
+      <FormControl>
+        <InputLabel>Type</InputLabel>
+        <Select value={type} onChange={(e) => setType(e.target.value)}>
+          <MenuItem value="restaurants">Restaurants</MenuItem>
+          <MenuItem value="attractions">Attractions</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
 };
 
 export default List;
